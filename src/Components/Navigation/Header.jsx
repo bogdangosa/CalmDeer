b8af31706322c33c1b3cea5b4f7e3a5d39676492
bogdangosa <bogdan.gosa@gmail.com>
2023-03-25 10:12:59 +0200

    mobile menu added and small changes to Our Work

--- src/Components/Navigation/Header.jsx
+++ src/Components/Navigation/Header.jsx
@@ -1,20 +1,29 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import RoundButton from '../Buttons/RoundButton';
 import './Header.css';
 
 function Header() {
+  const [HamburgerState,setHamburgerState] = useState(false);
 
 
   return (
     <header className="Header">
         <div className='header-container'>
             <p className='logo'>CalmDeer</p>
-            <ul className='nav-bar'>
-                <NavLink to='/' className={({ isActive }) => isActive ? "nav-links-active nav-links" : "nav-links"}><p data-text="Home">Home</p></NavLink>
-                <NavLink to='/OurWork' className={({ isActive }) => isActive ? "nav-links-active nav-links" : "nav-links"}><p data-text="Our Work">Our Work</p></NavLink>
-                <NavLink to='/News' className={({ isActive }) => isActive ? "nav-links-active nav-links" : "nav-links"}><p data-text="News">News</p></NavLink>
-                <NavLink to='/Contact' className='nav-contact-link'><RoundButton>Contact</RoundButton></NavLink>
+            <ul className={HamburgerState?'nav-bar':'nav-bar nav-bar-hidden'}>
+                <NavLink to='/' className={({ isActive }) => isActive ? "nav-links-active nav-links" : "nav-links"} onClick={()=>setHamburgerState(false)}><p data-text="Home">Home</p></NavLink>
+                <NavLink to='/OurWork' className={({ isActive }) => isActive ? "nav-links-active nav-links" : "nav-links"} onClick={()=>setHamburgerState(false)}><p data-text="Our Work">Our Work</p></NavLink>
+                <NavLink to='/News' className={({ isActive }) => isActive ? "nav-links-active nav-links" : "nav-links"} onClick={()=>setHamburgerState(false)}><p data-text="News">News</p></NavLink>
+                <NavLink to='/Contact' className='nav-contact-link' onClick={()=>setHamburgerState(false)}><RoundButton>Contact</RoundButton></NavLink>
             </ul>
+
+            <div className={HamburgerState?'hamburger-menu  hamburger-menu-opened':'hamburger-menu'} onClick={()=>setHamburgerState(!HamburgerState)}>
+              <div className='line line1'></div>
+              <div className='line line2'></div>
+              <div className='line line3'></div>
+            </div>
+
         </div>
     </header>
   )
