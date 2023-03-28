af4447629afd286a8ee544c728399be98019afaf
bogdangosa <bogdan.gosa@gmail.com>
2023-03-28 22:54:38 +0300

    update news to get data from hygraph

--- src/Pages/News.jsx
+++ src/Pages/News.jsx
@@ -1,16 +1,56 @@
+import { gql, GraphQLClient } from 'graphql-request';
+import { useEffect, useState } from 'react';
 import NewsCard from '../Components/Cards/NewsCard';
 import './News.css';
 
+const graphcms = new GraphQLClient("https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clfskro3j216101uefehihs30/master");
+
+
+const NEWSQUERY = gql`
+  query MultipleNews {
+    multipleNews {
+      createdAt
+      date
+      id
+      publishedAt
+      slug
+      title
+      updatedAt
+      content{
+        text
+      }
+      image{
+        url
+      }
+    }
+  }
+`
+
+
 function News() {
+    const [NewsData,setNewsData] = useState([]);
+
+    useEffect(()=>{
+        getNewsData();
+    },[])
+
+    const getNewsData = async ()=>{
+        const {multipleNews} = await graphcms.request(NEWSQUERY);
+        console.log(multipleNews);
+        setNewsData(multipleNews);
+
+    }
+
+
 
   return (
     <div className='News-background'>
       <div className='News'>
           <h1 className='news-title'>Latest News</h1>
           <div className='news-table-container'>
-            <NewsCard animation_delay={1} image="./image1.png" title="Praesent turpis libero, vestibulum ut faucibus nec, finibus sed quam. " description="Etiam dapibus est quis lacus eleifend volutpat. Etiam sed iaculis eros, et suscipit sapien. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas." date="02.03.2023"></NewsCard>
-            <NewsCard animation_delay={2} image="./image2.png" title="Praesent turpis libero, vestibulum ut faucibus nec, finibus sed quam. " description="Etiam dapibus est quis lacus eleifend volutpat. Etiam sed iaculis eros, et suscipit sapien. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas." date="02.03.2023"></NewsCard>
-            <NewsCard animation_delay={3} image="./image3.png" title="Praesent turpis libero, vestibulum ut faucibus nec, finibus sed quam. " description="Etiam dapibus est quis lacus eleifend volutpat. Etiam sed iaculis eros, et suscipit sapien. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas." date="02.03.2023"></NewsCard>
+            {NewsData.map((news_article,index)=>{
+                return <NewsCard key={index} animation_delay={index} image={news_article.image.url} title={news_article.title} description={news_article.content.text} date={news_article.date}></NewsCard>
+            })}
           </div>
       </div>
     </div>
